0ad5c58884d995282be15b12dfc8f8f1899d7aa1
Aleksi Rinta-Kauppila <ollialeksi.rk@gmail.com>
2015-07-27 10:35:34 +0300

    Initial commit

---
+++ js/controller.js
@@ -0,0 +1,20 @@
+var jaaraApp = angular.module('jaaraApp', []);
+
+jaaraApp.controller('JaaralaisetCtrl', function ($scope, $http) {
+  $http.get('http://crossorigin.me/http://www.nakedeye.fi/jaarappis/jaaralaiset.json')
+       .success(function(data) {
+         $scope.jaaralaiset = data;
+  });
+
+  $scope.compare = function(item) { 
+	  var query = ($scope.query || "").toLowerCase();
+	  var firstName = (item.firstname || "").toLowerCase();
+	  var lastName = (item.lastname || "").toLowerCase();
+	  var fullName = firstName + " " + lastName;
+	  
+	  return firstName.indexOf(query) === 0 || lastName.indexOf(query) === 0 || fullName.indexOf(query) === 0;
+  }
+
+});
+
+//http://crossorigin.me/http://www.nakedeye.fi/jaarapp/jaaralaiset.json
