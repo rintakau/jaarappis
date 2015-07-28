d10a19e4b49b154016788db7919d7efd405031c7
Heikki Ahlgren <heikki.ahlgren@sc5.io>
2015-07-28 16:31:22 +0300

    Implemented sticky searchbar & reformatted

--- js/controller.js
+++ js/controller.js
@@ -1,18 +1,29 @@
 var jaaraApp = angular.module('jaaraApp', []);
 
-jaaraApp.controller('JaaralaisetCtrl', function ($scope, $http) {
+jaaraApp.controller('JaaralaisetCtrl', function ($scope, $http, $document) {
   $http.get('http://crossorigin.me/http://www.nakedeye.fi/jaarappis/jaaralaiset.json')
-       .success(function(data) {
-         $scope.jaaralaiset = data;
+    .success(function(data) {
+      $scope.jaaralaiset = data;
+  });
+
+  var searchBar = angular.element(document.querySelector('.searchBarWrapper'));
+  var searchBarTop = searchBar.prop('offsetTop') - 10; // add margin
+  $document.bind('scroll', function(e) {
+    if (window.scrollY >= searchBarTop) {
+      searchBar.addClass('sticky');
+    } else {
+      searchBar.removeClass('sticky');
+    }
+
   });
 
   $scope.compare = function(item) { 
-	  var query = ($scope.query || "").toLowerCase();
-	  var firstName = (item.firstname || "").toLowerCase();
-	  var lastName = (item.lastname || "").toLowerCase();
-	  var fullName = firstName + " " + lastName;
-	  
-	  return firstName.indexOf(query) === 0 || lastName.indexOf(query) === 0 || fullName.indexOf(query) === 0;
+    var query = ($scope.query || "").toLowerCase();
+    var firstName = (item.firstname || "").toLowerCase();
+    var lastName = (item.lastname || "").toLowerCase();
+    var fullName = firstName + " " + lastName;
+    
+    return firstName.indexOf(query) === 0 || lastName.indexOf(query) === 0 || fullName.indexOf(query) === 0;
   }
 
 });
